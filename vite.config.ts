3294476e7a6ec6112b5f89c755b68a0b90f9f16e
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-03-04 07:18:18 -0300

    add configs
    I added vite configsm scss configs, base files, etc

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "src/assets/styles/variables";`,
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
 })
